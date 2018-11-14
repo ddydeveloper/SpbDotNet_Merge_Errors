8c9b21ab57c61777ec1d55443bd5a28a6431720c
Daniil <d.doronkin@itransition.com>
2018-11-14 12:14:10 +0300

    salted

--- Calculations/Calculator.cs
+++ Calculations/Calculator.cs
@@ -5,6 +5,10 @@ namespace Calculations
         public int SaltedSumm(int first, int second)
         {
             var salt = 10;
+            salt++;
+            salt+=2;
+            salt+=4;
+            salt++;
             salt++;            
             return first + second + salt;
         }
