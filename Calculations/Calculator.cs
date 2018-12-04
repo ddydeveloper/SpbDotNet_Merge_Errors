e6d42e4531be68694906c6abca7a17b85ecfebc8
Daniil <ddydeveloper@gmail.com>
2018-12-04 15:58:00 +0300

    modified

--- Calculations/Calculator.cs
+++ Calculations/Calculator.cs
@@ -2,10 +2,17 @@ namespace Calculations
 {
     public class Calculator
     {
+        // Me: what are you doing, method?
+        // Method: ¯\_(ツ)_/¯
         public static int SaltedSum(int first, int second)
         {
             var salt = 10;
             salt++;
+            salt += 2;
+
+            salt++;
+            salt++;
+            salt += 4;
 
             return first + second + salt;
         }
